b03e5c3f6342843648bef77e8faf939b82ea8854
mmirwaldt <michael.mirwaldt@gmail.com>
2020-03-10 19:12:05 +0100

    while loop introduced into reducer

--- src/main/java/net/mirwaldt/SequentialLoopAlchemicalReducer.java
+++ src/main/java/net/mirwaldt/SequentialLoopAlchemicalReducer.java
@@ -3,6 +3,11 @@ package net.mirwaldt;
 public class SequentialLoopAlchemicalReducer implements AlchemicalReducer {
     public String reduce(String polymer) {
         StringBuilder stringBuilder = new StringBuilder(polymer);
+        int indexOfNextChar = 1;
+        while(indexOfNextChar < stringBuilder.length()) {
+
+            indexOfNextChar++;
+        }
         return stringBuilder.toString();
     }
 }
