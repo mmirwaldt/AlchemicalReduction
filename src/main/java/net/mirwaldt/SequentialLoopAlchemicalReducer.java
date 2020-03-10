7e23588cad1b91ae1428dda1a5e010ffb5f2f055
mmirwaldt <michael.mirwaldt@gmail.com>
2020-03-10 19:10:00 +0100

    still failing test with one reduction

--- src/main/java/net/mirwaldt/SequentialLoopAlchemicalReducer.java
+++ src/main/java/net/mirwaldt/SequentialLoopAlchemicalReducer.java
@@ -2,6 +2,7 @@ package net.mirwaldt;
 
 public class SequentialLoopAlchemicalReducer implements AlchemicalReducer {
     public String reduce(String polymer) {
-        return polymer;
+        StringBuilder stringBuilder = new StringBuilder(polymer);
+        return stringBuilder.toString();
     }
 }
