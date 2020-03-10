53002e0827eb50605e050be6e1bc323898488b6e
mmirwaldt <michael.mirwaldt@gmail.com>
2020-03-10 19:24:55 +0100

    first attempt get test "one reduction" run

--- src/main/java/net/mirwaldt/SequentialLoopAlchemicalReducer.java
+++ src/main/java/net/mirwaldt/SequentialLoopAlchemicalReducer.java
@@ -3,11 +3,32 @@ package net.mirwaldt;
 public class SequentialLoopAlchemicalReducer implements AlchemicalReducer {
     public String reduce(String polymer) {
         StringBuilder stringBuilder = new StringBuilder(polymer);
-        int indexOfNextChar = 1;
-        while(indexOfNextChar < stringBuilder.length()) {
-
-            indexOfNextChar++;
+        int indexOfRightChar = 1;
+        while(indexOfRightChar < stringBuilder.length()) {
+            final char leftChar = stringBuilder.charAt(indexOfRightChar - 1);
+            final char rightChar = stringBuilder.charAt(indexOfRightChar);
+            if(canEliminate(leftChar, rightChar)) {
+                stringBuilder.delete(indexOfRightChar - 1, indexOfRightChar + 1);
+            }
+            indexOfRightChar++;
         }
         return stringBuilder.toString();
+    }
+
+    private boolean canEliminate(char leftChar, char rightChar) {
+        return isSameChar(leftChar, rightChar) && isOneLowerAndTheOtherUpperCase(leftChar, rightChar);
+    }
+
+    private boolean isOneLowerAndTheOtherUpperCase(char leftChar, char rightChar) {
+        return isLeftLowerAndRightUpperCase(leftChar, rightChar)
+                || isLeftLowerAndRightUpperCase(rightChar, leftChar);
+    }
+
+    private boolean isLeftLowerAndRightUpperCase(char leftChar, char rightChar) {
+        return Character.isLowerCase(leftChar) && Character.isUpperCase(rightChar);
+    }
+
+    private boolean isSameChar(char leftChar, char rightChar) {
+        return Character.compare(leftChar, rightChar) == 0;
     }
 }
